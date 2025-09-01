febfb89ae8a9ae2a0dc905f4ba5177c8299d5641
mc250041025-rgb <mc250041025@iiti.ac.in>
2025-09-01 19:26:11 +0530

    Add files via upload

---
+++ madhav.py
@@ -0,0 +1,32 @@
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha(): 
+           
+            if char.isupper():
+                result += chr((ord(char) - 65 + shift) % 26 + 65)
+           
+            else:
+                result += chr((ord(char) - 97 + shift) % 26 + 97)
+        else:
+           
+            result += char
+    return result
+
+
+
+statement = input("Enter your statement: ")
+shift = int(input("Enter shift value: "))
+
+
+encoded = caesar_cipher(statement, shift)
+print("Encoded Statement:", encoded)
+
+
+
+
+
+
+
+
+
